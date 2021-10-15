554f62f84f740464ded2180f62b5b0afedea42c2
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-10-15 16:36:13 +0200

    Initial commit with appSLice

---
+++ src/features/appSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { incrementByAmount } = appSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.apper.value)`
+export const selectapp = (state) => state.app.value;
+
+export default appSlice.reducer;
