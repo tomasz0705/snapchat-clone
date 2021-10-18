0e4bf9f4a18842de236159c02d73e7e3dab98f2b
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-10-18 18:13:31 +0200

    Added react-router dependencies to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Preview from './Preview';
 
 function App() {
   return (
     <div className="app">
-      <h1>LETS BUILD SNAPCHAT</h1>
-      <WebcamCapture />
+      <Router>
+        <div className="app__body">
+          <Switch>
+            <Route path="/preview">
+              <Preview />
+            </Route>
+            <Route exact path="/">
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
