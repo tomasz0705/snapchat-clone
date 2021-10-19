24f5ae31ab7dcebf91dd7e4faa9043a392d91e5a
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-10-19 22:19:48 +0200

    Added history replace / at useEffect

--- src/Preview.js
+++ src/Preview.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { selectCameraImage } from './features/cameraSlice';
 import "./Preview.css";
 
 function Preview() {
+    const cameraImage = useSelector(selectCameraImage);
+    const history = useHistory();
+
+    useEffect(() => {
+        if (!cameraImage) {
+            history.replace("/");
+        }
+    }, [cameraImage, history]);
+
     return (
         <div className="preview">
-            
+            <h2>THIS IS YOUR PREVIEW</h2>
+            <img src={cameraImage} alt="yourphoto" />
         </div>
     )
 }
