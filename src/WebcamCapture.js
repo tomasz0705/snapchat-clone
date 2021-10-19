76a15a86a554db0d15cc400f4a7644f0142fadbb
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-10-19 16:54:47 +0200

    Added styles to App.css & WebcamCapture.css

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -4,6 +4,7 @@ import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import { useDispatch } from 'react-redux';
 import { setCameraImage } from './features/cameraSlice';
 import { useHistory } from 'react-router-dom';
+import './WebcamCapture.css';
 
 const videoConstraints = {
     width: 250,
