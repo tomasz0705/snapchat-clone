10cb2a2feea001ee04a17f3e07e0fedc3e1c490c
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-10-21 21:42:18 +0200

    Added CloseIcon and handle closePreview using dispatch action

--- src/Preview.js
+++ src/Preview.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { selectCameraImage } from './features/cameraSlice';
+import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import "./Preview.css";
+import CloseIcon from '@mui/icons-material/Close';
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage);
     const history = useHistory();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (!cameraImage) {
@@ -14,9 +16,13 @@ function Preview() {
         }
     }, [cameraImage, history]);
 
+    const closePreview = () => {
+        dispatch(resetCameraImage());
+    }
+
     return (
         <div className="preview">
-            <h2>THIS IS YOUR PREVIEW</h2>
+            <CloseIcon onClick={closePreview} className="preview__close" />
             <img src={cameraImage} alt="yourphoto" />
         </div>
     )
