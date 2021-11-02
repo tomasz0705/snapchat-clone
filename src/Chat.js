b07a618fe9399eb57724d75ae84d16d8b4f91def
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-11-02 18:32:57 +0100

    Added HTML structure & timestamp in Chat,js

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,15 @@
+import { Avatar } from '@mui/material';
 import React from 'react';
 import './Chat.css';
 
 function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
     return (
         <div className="chat">
-            
+            <Avatar className="chat__avatar" src={profilePic} />
+            <div className="chat__info">
+                <h4>{username}</h4>
+                <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
+            </div>
         </div>
     )
 }
