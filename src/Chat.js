6ad892c0c4a55ce68fd2e63585bc59e8efe898ae
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-11-02 18:04:47 +0100

    Added posts with useEffect from db.collection

---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chat.css';
+
+function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
+    return (
+        <div className="chat">
+            
+        </div>
+    )
+}
+
+export default Chat
--- src/Chats.js
+++ src/Chats.js
@@ -1,26 +1,57 @@
-import { Avatar } from '@mui/material';
-import React from 'react';
-import './Chats.css';
-import SearchIcon from '@mui/icons-material/Search';
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import { Avatar } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import "./Chats.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import { db } from "./firebase";
+import Chat from "./Chat";
 
 function Chats() {
-    return (
-        <div className="chats">
-            <div className="chats__header">
-                <Avatar className="chats__avatar" />
-                <div className="chats__search">
-                    <SearchIcon />
-                    <input type="text" placeholder="Friends" />
-                </div>
-                <ChatBubbleIcon className="chats__chatIcon" />
-            </div>
+  const [posts, setPosts] = useState([]);
 
-            <div className="chat__posts">
-                
-            </div>
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+  return (
+    <div className="chats">
+      <div className="chats__header">
+        <Avatar className="chats__avatar" />
+        <div className="chats__search">
+          <SearchIcon />
+          <input type="text" placeholder="Friends" />
         </div>
-    )
+        <ChatBubbleIcon className="chats__chatIcon" />
+      </div>
+
+      <div className="chats__posts">
+        {posts.map(
+          ({
+            id,
+            data: { profilePic, username, timestamp, imageUrl, read },
+          }) => (
+            <Chat
+              key={id}
+              id={id}
+              username={username}
+              timestamp={timestamp}
+              imageUrl={imageUrl}
+              read={read}
+              profilePic={profilePic}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Chats
+export default Chats;
