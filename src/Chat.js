43979f3f833e52beaf7cbe44a6cba320c19a0ee3
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-11-03 16:52:58 +0100

    Added react timeago to chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Chat.css';
 import { Avatar } from '@mui/material';
 import StopRoundedIcon from '@mui/icons-material/StopRounded';
+import ReactTimeago from 'react-timeago';
 
 function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
     return (
@@ -9,7 +10,7 @@ function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
             <Avatar className="chat__avatar" src={profilePic} />
             <div className="chat__info">
                 <h4>{username}</h4>
-                <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
+                <p>Tap to view - <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} /></p>
             </div>
 
             {!read && <StopRoundedIcon className="chat__readIcon" />}
