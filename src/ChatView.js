6396c17a926790e4ce3f6848d8b0da3c6ec8ea0a
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-11-12 21:56:43 +0100

    Added react countdown circle timer to chatView

--- src/ChatView.js
+++ src/ChatView.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import "./ChatView.css";
@@ -21,6 +22,27 @@ function ChatView() {
     return (
         <div className="chatView">
             <img src={selectedImage} onClick={exit} alt="your" />
+            <div className="chatView__timer">
+                <CountdownCircleTimer
+                    isPlaying
+                    duration={10}
+                    strokeWidth={6}
+                    size={50}
+                    colors={[
+                        ['#004777', 0.33],
+                        ['#F7B801', 0.33],
+                        ['#A30000', 0.33],
+                    ]}
+                >
+                    {({ remainingTime }) => {
+                        if(remainingTime === 0) {
+                            exit();
+                        }
+                        return remainingTime;
+                    }}
+                </CountdownCircleTimer>
+            </div>
+
         </div>
     )
 }
